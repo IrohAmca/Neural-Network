1e5fd3f11622b5b0eaf045af7b28304ea3187fd7
Muhammet Ali Aygün <109526680+IrohAmca@users.noreply.github.com>
2024-09-22 23:08:58 +0300

    Added 'add' Layer util

--- MLP.py
+++ MLP.py
@@ -15,25 +15,33 @@ class Module:
     
 class Neuron(Module):
 
-    def __init__(self, nin, nonlin=True):
+    def __init__(self, nin, activation='Linear'):
         self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
         self.b = Value(0)
-        self.nonlin = nonlin
-
+        self.activation = activation
+        
     def __call__(self, x):
         act = sum((wi*xi for wi,xi in zip(self.w, x)), self.b)
-        return act.relu() if self.nonlin else act
+
+        if self.activation == 'ReLU':
+            return act.relu()
+        elif self.activation == 'Tanh':
+            return act.tanh()
+        elif self.activation == 'Linear':
+            return act
+        else:
+            raise ValueError(f"Unknown activation: {self.activation}")
 
     def parameters(self):
         return self.w + [self.b]
 
     def __repr__(self):
-        return f"{'ReLU' if self.nonlin else 'Linear'}Neuron({len(self.w)})"
+        return f"{self.activation} Neuron({len(self.w)})"
 
 class Layer(Module):
 
-    def __init__(self, nin, nout, **kwargs):
-        self.neurons = [Neuron(nin, **kwargs) for _ in range(nout)]
+    def __init__(self, nin, nout, activation='Linear'):
+        self.neurons = [Neuron(nin,activation) for _ in range(nout)]
 
     def __call__(self, x):
         out = [n(x) for n in self.neurons]
@@ -47,10 +55,12 @@ class Layer(Module):
 
 class MLP(Module):
 
-    def __init__(self, nin, nouts):
-        sz = [nin] + nouts
-        self.layers = [Layer(sz[i], sz[i+1], nonlin=i!=len(nouts)-1) for i in range(len(nouts))]
+    def __init__(self):
+        self.layers = []
 
+    def add(self,input, output, activation='Linear'):
+        self.layers.append(Layer(input, output, activation=activation))
+        
     def __call__(self, x):
         for layer in self.layers:
             x = layer(x)
